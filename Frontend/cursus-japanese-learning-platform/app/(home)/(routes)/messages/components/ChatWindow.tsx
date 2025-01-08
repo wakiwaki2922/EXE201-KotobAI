33d622290ce0032a7006cb376b3793c3ba973a82
GiangDam92 <wyzeman.2922@gmail.com>
2025-01-08 21:00:35 +0700

    Update codebase #4

--- Frontend/cursus-japanese-learning-platform/app/(home)/(routes)/messages/components/ChatWindow.tsx
+++ Frontend/cursus-japanese-learning-platform/app/(home)/(routes)/messages/components/ChatWindow.tsx
@@ -95,7 +95,7 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ chatId }) => {
 
     const getChatbotResponse = async (message: string) => {
         try {
-            const apiKey = "AIzaSyBpwEbwSuVb1Aod6FNnoPVVOh53oRFZ3wU";
+            const apiKey = "";
             const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-002:generateContent?key=${apiKey}`, {
                 method: 'POST',
                 headers: {
